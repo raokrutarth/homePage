0458969d663e46767ad5a7ea9f8c6ca107dfa5bb
raokrutarth <krk91@outlook.com>
2016-07-09 17:40:57 -0400

    old resume

---
+++ js/config.js
@@ -0,0 +1,20 @@
+/*
+	Halcyonic 3.1 
+*/
+
+window._skel_config = {
+	preset: 'standard',
+	prefix: 'css/style',
+	resetCSS: true,
+	breakpoints: {
+		'1000px': {
+			grid: {
+				gutters: 25
+			}
+		}
+	}
+};
+
+window._skel_panels_config = {
+	preset: 'standard'
+};
